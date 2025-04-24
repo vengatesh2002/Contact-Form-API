0c1c39982eab6243e5d6aacb00fd5b2682d60df9
VENGATESH HARI PRABU J <109472405+vengatesh2002@users.noreply.github.com>
2025-04-24 10:15:54 +0530

    server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
